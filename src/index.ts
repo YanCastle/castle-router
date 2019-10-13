6f13327ee18c33276b5ea8719d1a30f81b2e8bac
castleiMac <castle@tansuyun.cn>
2019-10-13 23:51:32 +0800

    route check

--- src/index.ts
+++ src/index.ts
@@ -71,6 +71,9 @@ export async function check(ctx: any) {
  */
 export async function controller(ctx: any, route: { Module?: string, Method: string, Path?: string, Controller: string } | string = "", data?: any) {
     // let route = ctx.route;
+    if (!ctx.route) {
+        await ctx.config.getController();
+    }
     let cr = Object.assign(ctx.route);
     if ("string" == typeof route && route.length > 0) {
         ctx.path = route;
